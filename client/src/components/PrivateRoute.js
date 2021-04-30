1dd150d55072b7e5d9b0a1599f8bd75922e6f0b6
aprilyanggarwood <yangyyj822@hotmail.com>
2021-04-29 18:15:07 -0700

    working on firebase for authisidicator

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,35 @@
+// import React from "react";
+// import { Route, Redirect } from "react-router-dom";
+// import { useAuth } from "../contexts/AuthContext";
+// export default function PrivateRoute({ component: Comp, ...rest }) {
+//   const { currentUser } = useAuth();
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) => {
+//         return currentUser ? <Comp {...props} /> : <Redirect to="/login" />;
+//       }}
+//     ></Route>
+//   );
+// }
+
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+  const { currentUser } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return currentUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    ></Route>
+  );
+}
